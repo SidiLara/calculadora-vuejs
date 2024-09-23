9cc340dc6920a5ff79c5747c63fa2063d592b4c3
Sidinei de Souza Lara <sidineilara@gmail.com>
2024-09-23 20:31:36 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+const valor1 = 0;
+const valor2 = 0;
+const resultado = 0;
+
+function resultadoCalculo() {
+  return `${resultado}`
+}
+
+function 
+
+</script>
+
+<template>
+  <center>
+
+    <form>
+      <input class="form-control" id="valor1" :type="number" placeholder="Valor 1" />
+      <select>selectCalc()</select>
+      <input :type="number" placeholder="Valor 2" />
+      <br />
+      <span style="font-size: 48px;">{{ resultado }}</span>
+    </form>
+
+  </center>
+</template>
+
+<style scoped></style>
